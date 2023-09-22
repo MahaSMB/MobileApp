7c06d09097180c858bfc05bfbf0e7460fffd3869
Maha Basheikh <34198949+MahaSMB@users.noreply.github.com>
2023-09-22 16:38:41 -0400

    Submitted files for Lab 2

---
+++ iOS-Lab2/iOS-Lab-2/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  iOS-Lab-2
+//
+//  Created by user248611 on 9/22/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
