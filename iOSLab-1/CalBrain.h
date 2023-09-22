c7a20027d6d166ece4f73d33549bc4a245419a44
Maha Basheikh <34198949+MahaSMB@users.noreply.github.com>
2023-09-22 17:02:42 -0400

    Including zip file

--- iOSLab-1/CalBrain.h
+++ iOSLab-1/CalBrain.h
@@ -2,7 +2,7 @@
 //  CalBrain.h
 //  Sep21_Calculator
 //
-//  Created by mays on 2023-09-21.
+//  Created by Maha Basheikh on 2023-09-21.
 //
 
 #import <Foundation/Foundation.h>
@@ -10,9 +10,10 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CalBrain : NSObject
-// declare function to push number  to array
+// declared function to push number  to NSMutableArray
 -(void)pushItem :(double)number;
-// declare function to calcalate based on the oprator
+
+// declared function to calculate based on the operator
 -(double)calculate:(NSString*)opr;
 
 @end
