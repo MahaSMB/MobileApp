6f4fb22f4a25f3b235d6633b288aa211c240b02b
Maha Basheikh <34198949+MahaSMB@users.noreply.github.com>
2023-10-10 18:11:24 -0400

    Got images working with Picker View

--- ImageViewer/Model.swift
+++ ImageViewer/Model.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class Model {
     lazy var listOfImages = ["Sunset":"https://images.freeimages.com/images/large-previews/1c9/maine-at-4-45-am-1370871.jpg",
-                             "Sunrise": "https://www.freeimages.com/photo/sunrise-beach-1516936",
-                             "Jupiter": "https://www.freeimages.com/photo/jupiter-1153866",
-                             "Mars": "https://www.freeimages.com/photo/mars-2426728",
-                             "Saturn": "https://www.freeimages.com/photo/saturn-in-the-galaxy-2284016"]    
+                             "Sunrise": "https://images.freeimages.com/images/large-previews/07c/sunrise-beach-1516936.jpg",
+                             "Jupiter": "https://images.freeimages.com/images/large-previews/e06/jupiter-1153866.jpg",
+                             "Mars": "https://images.freeimages.com/images/large-previews/9cc/red-planet-rise-1629776.jpg",
+                             "Saturn": "https://images.freeimages.com/images/large-previews/a76/saturn-hd-planet-1152093.jpg"]
     
 }
--- ImageViewer/ViewController.swift
+++ ImageViewer/ViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    //var URLStr: URL?
+    lazy var myDictionary = Model()
+    //lazy var sharedService = Service()
+    
+    lazy var myList = Array(myDictionary.listOfImages)
 
-
+    @IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,22 +23,39 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     // Setting number of components of picker view
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1;
+        return 1
     }
     
     // Setting number of rows in Picker View
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5;
+        return myList.count
     }
    
-    // 
-     
     
     // Getting data for Picker View
     func pickerView(_ pickerView: UIPickerView,
                  titleForRow row: Int,
                forComponent component: Int) -> String? {
-        return "String"
+        return myList[row].key
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    didSelectRow row: Int,
+                    inComponent component: Int) {
+        //let rowStr = String (myList[row].key)
+        let myURLStr = (myList[row].value)  // prints the value of the myDictionary
+
+        //print(myURLStr) // testing to see what myURLStr prints
+                
+        Service.shared.getImage(urlStr: myURLStr) { data in
+            if let data = data {
+                DispatchQueue.main.async {
+                    [unowned self] in
+                    self.myImage.image = UIImage(data: data)
+                }
+            }
+        }
+        
     }
     
 }
