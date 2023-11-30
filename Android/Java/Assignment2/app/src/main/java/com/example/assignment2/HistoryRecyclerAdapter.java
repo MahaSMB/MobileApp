dfd6ba6f5d005d61539080cfa82fecb2376eafd9
MahaSMB <34198949+MahaSMB@users.noreply.github.com>
2023-11-29 23:04:20 -0500

    Android Assignment 2
    - Finally got the Recycler View's Purchase History screen to show up and work!
    -- formatted price to two decimal places

--- Android/Java/Assignment2/app/src/main/java/com/example/assignment2/HistoryRecyclerAdapter.java
+++ Android/Java/Assignment2/app/src/main/java/com/example/assignment2/HistoryRecyclerAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class HistoryRecyclerAdapter extends
@@ -36,20 +37,21 @@ public class HistoryRecyclerAdapter extends
     @NonNull
     @Override
     public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.history_list_row, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.history_list_row_update, parent, false);
         return new HistoryViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
-        TextView historyProductName = holder.itemView.findViewById(R.id.tvRvProductName);
-        TextView historyProductQty = holder.itemView.findViewById(R.id.tvRvProductQty);
-        TextView historyProductPrice = holder.itemView.findViewById(R.id.tvRvProductPrice);
-        TextView historyProductDate = holder.itemView.findViewById(R.id.tvRvProductDate);
+        TextView historyProductName = holder.itemView.findViewById(R.id.tvRvProductNameUpdate);
+        TextView historyProductQty = holder.itemView.findViewById(R.id.tvRvProductQtyUpdate);
+        TextView historyProductPrice = holder.itemView.findViewById(R.id.tvRvProductPriceUpdate);
+        TextView historyProductDate = holder.itemView.findViewById(R.id.tvRvProductDateUpdate);
 
         historyProductName.setText(historyList.get(position).getProductName());
-        historyProductQty.setText(historyList.get(position).getProductQty());
-        historyProductPrice.setText(String.valueOf(historyList.get(position).getProductPrice()));
+        historyProductQty.setText(String.valueOf(historyList.get(position).getProductQty()));
+        DecimalFormat df = new DecimalFormat("0.00");
+        historyProductPrice.setText(String.valueOf(df.format(historyList.get(position).getProductPrice())));
         historyProductDate.setText(String.valueOf(historyList.get(position).getPurchaseDate()));
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
